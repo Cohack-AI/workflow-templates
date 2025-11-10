3121139ea85d960416c536a087a48d5422abe449
Abhishek <cohackai@gmail.com>
2025-11-10 22:22:50 +0530

    xxx

--- defaults/http/src/workflow.ts
+++ defaults/http/src/workflow.ts
@@ -14,7 +14,7 @@ type Output = z.input<typeof output>;
 class SampleWorkflow extends cohack.BaseWorkflow<Input, Output> {
     constructor() {
         const client = cohack.createCohackClient();
-        super('greeting', input, output, client);
+        super(input, output, client);
     }
 
     async run(input: Input): Promise<Output> {
